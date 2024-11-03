1ad45c026eba72b968bfe489897a38bc257f1812
Positive-x7 <soltagireevt@gmail.com>
2024-11-03 12:16:52 +0300

    Add files via upload

---
+++ pandas/your_code.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Загружаем датафрейм 
+df = pd.read_csv('GoogleApps_1606229122.csv')
+
+
+print(df.describe())
+
+#print(df.tail())
+
+#print(df.head())
+
+#print(df.info())
